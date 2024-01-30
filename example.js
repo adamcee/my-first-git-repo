d536f4f75618f39203969055be63d76d34400126
Adam Cahan <adam.cahan@gmail.com>
2024-01-30 11:56:53 -0600

    updated example.s

--- example.js
+++ example.js
@@ -1,3 +1,13 @@
 console.log('goodbye')
 console.log('its snowing')
 console.log('a lot')
+
+console.log("hello");
+
+let sum = 0;
+
+for (let i = 0; i < 5; i++) {
+  sum += i;
+}
+
+console.log(sum);
